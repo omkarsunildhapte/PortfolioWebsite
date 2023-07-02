e807f46a51e3c268a3f571a3a11836ec2f7c8fcc
omkarsunildhapte <omkardhapte99@gmail.com>
2023-07-02 21:01:02 +0530

    the contact and footer are the in bootstrap

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  openEmailClient(emailId: string): void {
+    const mailtoUrl = `mailto:${emailId}`;
+    window.open(mailtoUrl);
+  }
+  openPhoneDialer(phoneNumber: string): void {
+    const telUrl = `tel:${phoneNumber}`;
+    window.open(telUrl);
+  }
 
 }

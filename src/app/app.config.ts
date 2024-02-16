32bf73fc07fae22b34169e82efead1f85b13eb8e
rizworksOmkar <67897741+omkarsunildhapte@users.noreply.github.com>
2024-02-17 01:01:00 +0530

    added servies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyBg-_jwvWDWvVIKQCW5C3deMvn6XbNcehg",
+  authDomain: "omkar-dhapte-developer.firebaseapp.com",
+  projectId: "omkar-dhapte-developer",
+  storageBucket: "omkar-dhapte-developer.appspot.com",
+  messagingSenderId: "371167500697",
+  appId: "1:371167500697:web:a297ad2f2ab2bc4e1c5b08",
+  measurementId: "G-HFTN6B5R5W"
+}
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes),
+  provideClientHydration(),
+  importProvidersFrom([
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ])
+  ]
 };

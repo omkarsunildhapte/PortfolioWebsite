a847bf0465d09a48c9175e366a41d43acc70a599
rizworksOmkar <67897741+omkarsunildhapte@users.noreply.github.com>
2024-02-17 15:35:39 +0530

    changes the dynamic

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  portfolioItems = [
+    { id: 1, title: 'App 1', category: 'App', image: 'portfolio-1.jpg' },
+    { id: 2, title: 'Web 3', category: 'Web', image: 'portfolio-2.jpg' },
+    { id: 3, title: 'App 2', category: 'App', image: 'portfolio-3.jpg' },
+  ];
 }
--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,12 +1,39 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-testimonials',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './testimonials.component.html',
-  styleUrl: './testimonials.component.scss'
+  styleUrl: './testimonials.component.scss',
+  animations: [
+    trigger('slideAnimation', [
+      transition(':enter', [
+        query('.testimonial-item', [
+          style({ opacity: 0, transform: 'translateX(-50px)' }),
+          stagger(100, [
+            animate('0.5s ease-out', style({ opacity: 1, transform: 'none' }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class TestimonialsComponent {
-
+  testimonials = [
+    {
+      name: 'Saul Goodman',
+      position: 'Ceo & Founder',
+      image: '../../assets/img/testimonials/testimonials-1.jpg',
+      quote: 'Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.'
+    },
+    {
+      name: 'Sara Wilsson',
+      position: 'Designer',
+      image: '../../assets/img/testimonials/testimonials-2.jpg',
+      quote: 'Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.'
+    },
+  ];
 }

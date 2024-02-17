d50502ea8dd6dbe6c338e2bf1f67df5804dcd30a
rizworksOmkar <67897741+omkarsunildhapte@users.noreply.github.com>
2024-02-17 15:47:38 +0530

    replace the chunks

--- www/server/main.server.mjs
+++ www/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-K6254G57.mjs";import"./chunk-6UANKU6L.mjs";import"./chunk-COT65Y5O.mjs";export{a as default};
+import{a}from"./chunk-G6UIOB7R.mjs";import"./chunk-MBSUQG45.mjs";import"./chunk-COT65Y5O.mjs";export{a as default};
